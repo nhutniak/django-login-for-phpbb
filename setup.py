ba6d55602a78b62579ab830d6c09f35866a71fc8
Sergei Maertens <sergeimaertens@gmail.com>
2013-11-08 18:44:47 +0100

    cleaning up project layout

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+        name='sessionprofile',
+        version='1.0',
+        packages=find_packages(),
+        package_dir = {'': 'django/sessionprofile'}
+)
